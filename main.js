f448b2b7640fd81badb894213058c542fd570b98
computermaster0101 <computermaster0101@users.noreply.github.com>
2022-05-01 15:36:47 -0700

    trying to catch onclick event

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const electron = require('electron')
 const url = require('url')
 const path = require('path')
 
+process.env.NODE_ENV = 'development'
+
 const {app, BrowserWindow, Menu, ipcMain} = electron
 let mainWindow
  
@@ -13,20 +15,25 @@ app.on('ready', function(){
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'mainWindow.html'),
     protocol: 'file:',
-    slashes: true
-  }))
+    slashes:true
+  }));
+  // Quit app when closed
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
   
   //Build menu from template
-  const mainMenu = Menu.buildFromTemplate(mainMenuTempate)
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
   //Insert menu
   Menu.setApplicationMenu(mainMenu)
 })
 
-ipcMain.on('fullyLoaded',function(e){
-	console.log("printing a message")
-})
+ipcMain.on('hit', function(e){
+  addWindow.close(); 
+  console.log("hit!")
+});
 
-const mainMenuTempate = [
+const mainMenuTemplate = [
   {
     label: 'File',
 	submenu: [
@@ -47,3 +54,26 @@ const mainMenuTempate = [
 	]
   }
 ]
+
+// If OSX, add empty object to menu
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
+}
