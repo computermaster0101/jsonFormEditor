81fd520803bbb5e196ae828e7258931eb251e239
computermaster0101 <computermaster0101@users.noreply.github.com>
2022-05-01 14:39:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,44 @@ const electron = require('electron')
 const url = require('url')
 const path = require('path')
 
-const {app, BrowserWindow} = electron
-
- let mainWindow
+const {app, BrowserWindow, Menu} = electron
+let mainWindow
  
- // listen for the app to be ready
+// listen for the app to be ready
 app.on('ready', function(){
-	//create new window
-	mainWindow = new BrowserWindow({})
-	//load html file into the window
-	mainWindow.loadURL(url.format({
-		pathname: path.join(__dirname, 'mainWindow.html'),
-		protocol: 'file:',
-		slashes: true
-	}))
+  //create new window
+  mainWindow = new BrowserWindow({})
+  //load html file into the window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  
+  //Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTempate)
+  //Insert menu
+  Menu.setApplicationMenu(mainMenu)
 })
+
+const mainMenuTempate = [
+  {
+    label: 'File',
+	submenu: [
+	  {
+	    label: 'Open Directory',
+		accelerator: process.platform == 'darwin' ? 'Command+O' : 'Ctrl+O',
+		click(){
+			app.quit()
+		}
+	  },
+	  {
+		label: 'Quit',
+		accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+	    click(){
+		  app.quit()
+	    }
+	  }
+	]
+  }
+]
