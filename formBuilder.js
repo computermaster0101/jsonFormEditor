8905df099e37e234fa4bbf3f59a776bc3e88e514
computermaster0101 <computermaster0101@users.noreply.github.com>
2022-05-14 21:16:32 -0700

    Update formBuilder.js

--- formBuilder.js
+++ formBuilder.js
@@ -1,6 +1,7 @@
 exports.getFormFromJSON = (object) => {return buildForm(object)}
 
 const form = document.createElement('form')
+const br = document.createElement("br")
 
 function buildForm(object){
 	form.innerHTML = "myFirstForm"
@@ -12,6 +13,7 @@ function expandObject(object){
 	Object.keys(object).forEach(key => {
 		if(object[key] == null ){
 			addNullToForm(key)
+			addTypeListToForm(key,null)
 		} else if(typeof object[key] === 'string') {
 			addStringToForm(key,object[key])
 		} else if(typeof object[key] === 'number'){
@@ -25,8 +27,26 @@ function expandObject(object){
 }
 
 function addNullToForm(key){
+	form.appendChild(br.cloneNode())
 	console.log(`key: ${key}`, typeof(key),`object: null`)
+	label = document.createElement('label')
+	input = document.createElement('input')
+	strong = document.createElement('strong')
+	strong.innerHTML = key
+	label.appendChild(strong)
+	setAttr(label ,{
+			"id": "1",
+			"for": "null"
+		})
+	setAttr(input,{
+		"type": "text",
+		"name": "null",
+		"placeholder": "null",
+		"readonly": "true"
+	})
+	appendElementsToForm([label, input])
 }
+
 function addStringToForm(key,value){
 	console.log(`key: ${key}`, typeof(key),`object: ${value}`, typeof(value))
 }
@@ -39,4 +59,27 @@ function addBooleanToForm(key,value){
 function addObjectToForm(key,object){
 	console.log(`key: ${key}`, typeof(key),`object: ${JSON.stringify(object)}`, typeof(object))
 	expandObject(object)
+}
+
+function setAttr(element , attributes){
+	Object.keys(attributes).forEach(key => {
+		element.setAttribute(key , attributes[key])
+	})
+}
+
+function appendElementsToForm(elements){
+	elements.forEach(element => {
+		form.appendChild(element)
+	})
+}
+
+function addTypeListToForm(key,type){
+	console.log(key,type)
+	select = document.createElement('select')
+	string = document.createElement('option')
+	number = document.createElement('option')
+	bool = document.createElement('option')
+	object = document.createElement('option')
+	list = document.createElement('option')
+
 }
