bf28fa654e4d3e7619153400d95b3eb3341c2ece
computermaster0101 <computermaster0101@users.noreply.github.com>
2022-05-16 20:54:33 -0700

    Update formBuilder.js

--- formBuilder.js
+++ formBuilder.js
@@ -1,62 +1,95 @@
-exports.getFormFromJSON = (object) => {return buildForm(object)}
+exports.getFormFromJSON = (object) => {return buildFormFromObject(object)}
 
-const form = document.createElement('form')
-
-function buildForm(object){
-	form.innerHTML = ""
+function buildFormFromObject(object){
+	
+	form = document.createElement('form')
+	ul = document.createElement('ul')
+	form.appendChild(ul)
 	expandObject(object)
+	
 	return form
+}
+
+function uuid(){
+	var dt = new Date().getTime();
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = (dt + Math.random()*16)%16 | 0;
+		dt = Math.floor(dt/16);
+		return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+	});
+	return uuid;
+}
+	
+function buildListItem(key,value,type){
+	li = document.createElement('li')
+	div = document.createElement('div')
+	label = document.createElement('label')
+	strong = document.createElement('strong')
+	input = document.createElement('input')
+
+	setAttr(strong, {
+		"innerHTML": key
+	})
+	setAttr(label ,{
+		"id": uuid(),
+		"for": `${key}-${value}`
+	})
+	setAttr(input,{
+		"type": type,
+		"name": `${key}-${value}`,
+		"placeholder": value
+	})
+	if((type == "checkbox") && (value)){setAttr(input,{"checked":value})}
+	li.appendChild(div)
+	div.appendChild(label)
+	label.appendChild(strong)
+	div.appendChild(input)
+	return li
 }
 
 function expandObject(object){
 	Object.keys(object).forEach(key => {
 		if(object[key] == null ){
-			addInputToForm(key,"null")
+			
+			buildListItem(key,object[key],"input")
+			ul.appendChild(li)
+	
 		} else {
 			switch (typeof(object[key])){
-				case 'string':
-					addInputToForm(key,object[key])
-					break
-				case 'number':
-					addInputToForm(key,object[key])
-					break
-				case 'boolean':
-					addInputToForm(key,object[key])
-					break
 				case 'object':
-					addInputToForm(key,"object")
+					li = buildListItem(key,object[key],"input")
+					ul.appendChild(li)
 					expandObject(object[key])
 					break
+					
+				case 'string':
+					li = buildListItem(key,object[key],"input")
+					ul.appendChild(li)
+					break
+					
+				case 'number':
+					li = buildListItem(key,object[key],"number")
+					ul.appendChild(li)
+					break
+					
+				case 'boolean':
+					li = buildListItem(key,object[key],"checkbox")
+					ul.appendChild(li)
+					break
+					
 			}
 		}
+		
 	})
 }
-
-function addInputToForm(key,value){
-	console.log(`key: ${key}`, typeof(key),`object: null`)
-	form.appendChild(document.createElement("br"))
-	label = document.createElement('label')
-	input = document.createElement('input')
-	strong = document.createElement('strong')
-	strong.innerHTML = key
-	label.appendChild(strong)
-	setAttr(label ,{
-			"id": "1",
-			"for": "null"
-		})
-	setAttr(input,{
-		"type": "text",
-		"name": `${key}.${value}`,
-		"placeholder": value,
-		"readonly": "true"
-	})
-	appendElementsToForm([label, input])
-}
-
 
 function setAttr(element , attributes){
 	Object.keys(attributes).forEach(key => {
-		element.setAttribute(key , attributes[key])
+		if(key == "innerHTML"){
+			element.innerHTML = attributes[key]
+		} else {
+			element.setAttribute(key , attributes[key])
+		}
 	})
 }
 
@@ -64,15 +97,4 @@ function appendElementsToForm(elements){
 	elements.forEach(element => {
 		form.appendChild(element)
 	})
-}
-
-function addTypeListToForm(key,type){
-	console.log(key,type)
-	select = document.createElement('select')
-	string = document.createElement('option')
-	number = document.createElement('option')
-	bool = document.createElement('option')
-	object = document.createElement('option')
-	list = document.createElement('option')
-
 }
